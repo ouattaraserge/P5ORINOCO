c89b5c8e9b8e4af1bc579f65894d0f84c1eed129
sergeo225 <ouattaraserge@hotmail.com>
2020-11-24 10:24:05 +0100

    ajout bouton mes achats dans page index.html

--- front/panier.js
+++
@@ -1,7 +0,0 @@
-let total = 0;
-//montrons si le panier contient un article
-function contenuPanier () {
-    if (contenuPanier.length > 0) {
-        document.getElementById("panierVide").remove();
-    }
-}
